4965a26433fddb30e6e21b4d1236063671e460a7
Drarkors <rafaelfzanetti@hotmail.com>
2024-11-07 20:16:32 -0300

    🧪 Broken tests due to env being resetted

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,3 +1,46 @@
-test('1 + 1', () => {
-  expect(1 + 1).toBe(2)
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('Create account (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    const config = moduleRef.get(ConfigService)
+
+    console.log('BD:', config.get('DATABASE_URL'))
+
+    await app.init()
+  })
+
+  test('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    })
+
+    console.log(response.body)
+    expect(response.statusCode).toEqual(201)
+
+    const userOnDatabae = await prisma.user.findUnique({
+      where: {
+        email: 'johndoe@email.com',
+      },
+    })
+
+    expect(userOnDatabae).toBeTruthy()
+  })
 })
--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -22,6 +22,8 @@ const schemaId = randomUUID()
 beforeAll(async () => {
   const databaseURL = generateUniqueDatabaseURL(schemaId)
 
+  console.log(databaseURL)
+
   process.env.DATABASE_URL = databaseURL
 
   execSync('npx prisma migrate deploy')
