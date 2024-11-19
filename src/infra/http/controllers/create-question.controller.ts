a2b960e84275a09f61e45e6d445902802f8614d0
Drarkors <rafaelfzanetti@hotmail.com>
2024-11-19 20:06:52 -0300

    🧩Implementing CreateQuestionUseCase and QuestionsRepository on Nest

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -2,9 +2,9 @@ import { z } from 'zod'
 import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { ZodValidationPipe } from '../pipes/zod-validation.pipe'
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { UserPayload } from '@/infra/auth/jwt.strategy'
 import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
@@ -18,21 +18,7 @@ type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor(private prisma: PrismaService) {}
-
-  private convertToSlug(text: string) {
-    const slugText = text
-      .normalize('NFKD')
-      .toLowerCase()
-      .trim()
-      .replace(/\s+/g, '-') // replaces all(/g) whitespaces (\s+) with dashes "-"
-      .replace(/[^\w-]+/g, '') // removes all(/g) non(^) words ([\w-]+)
-      .replace(/_+/g, '-') // replaces all(/g) underlines(/_+) with dashes "-"
-      .replace(/--+/g, '-') // replaces all(/g) double dashes (/--) with one dash "-" (may be caused by the underline replacement)
-      .replace(/-$/, '') // removes all(/g) dash(/-) at the end($) of our string (eg.: removing-ending-dash-)
-
-    return slugText
-  }
+  constructor(private createQuestion: CreateQuestionUseCase) {}
 
   @Post()
   async handle(
@@ -43,17 +29,11 @@ export class CreateQuestionController {
     const { sub: userId } = user
     const { title, content } = body
 
-    const slug = this.convertToSlug(title)
-
-    await this.prisma.question.create({
-      data: {
-        title,
-        content,
-        slug,
-        authorId: userId,
-      },
+    await this.createQuestion.execute({
+      title,
+      content,
+      authorId: userId,
+      attachmentsIds: [],
     })
-
-    return 'ok'
   }
 }
